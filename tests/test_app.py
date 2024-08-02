69fa117119c4758d98dfdf4b81ece7200f685ed6
amanmdest <amanmdest@gmail.com>
2024-08-01 21:13:48 -0300

    Trabalhando com schemas

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,38 @@
 from http import HTTPStatus
 
+import pytest
 from fastapi.testclient import TestClient
 
 from fast_zero.app import app
 
 
-def test_read_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # Arrange
+@pytest.fixture
+def client():
+    return TestClient(app)
 
+
+def test_read_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')  # Act
 
     assert response.status_code == HTTPStatus.OK  # Assert
-    assert response.json() == {'message': 'Olar Mundis!'}
+    assert response.json() == {'message': 'Olar Mundis!'}  # Assert
+
+
+def test_create_user(client):
+    response = client.post(  # UserSchema
+        '/users/',
+        json={
+            'username': 'testusername',
+            'password': 'password',
+            'email': 'test@test.com',
+        },
+    )
+
+    # Validar status code
+    assert response.status_code == HTTPStatus.CREATED
+    # Validar UserPublic
+    assert response.json() == {
+        'username': 'testusername',
+        'email': 'test@test.com',
+        'id': 1,
+    }
